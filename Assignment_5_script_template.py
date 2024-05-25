99a9f6f8a5fbefb2994a29918f93886c17463d74
Olatunde Mosobalaje <olatunde.mosobalaje@covenantuniversity.edu.ng>
2024-05-25 17:16:18 +0100

    Create Assignment_5_script_template.py

---
+++ Assignment_5_script_template.py
@@ -0,0 +1,52 @@
+# A script to classify and count 'active' and 'inactive'
+# gridblocks in a discretized reservoir
+
+#################### Task 1 ############################
+    # Request for reservoir dimensions and discretization parameters
+
+# Lx
+pass
+# Ly
+pass
+# Lz
+pass
+# nx
+pass
+# ny
+pass
+# nz
+pass
+
+
+#################### Task 2 ############################
+    # Request for the cut-off value
+# cut_off
+pass
+
+#################### Task 3 ############################
+    # Initialize counters
+
+# n_active
+pass
+# n_inactive
+pass
+
+#################### Task 4 ############################
+    # Loop through all blocks (nested loop)
+for k in range(1, nz+1):
+    # Initialize layer counter
+    pass
+    # two nested loops go here
+    pass
+
+    # Print layer count
+    pass
+
+#################### Task 5 ############################
+    # Print overall counts
+
+# Print 'active'
+pass
+
+# Print 'inactive'
+pass
