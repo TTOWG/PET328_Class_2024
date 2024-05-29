75eb7f82cf9071dad35b4e233a36b99b0692b91a
EsumehRiwo <165013092+EsumehRiwo@users.noreply.github.com>
2024-05-29 20:57:07 +0100

    Update Assignment_5_script_template.py

--- Assignment_5_script_template.py
+++ Assignment_5_script_template.py
@@ -2,51 +2,57 @@
 # gridblocks in a discretized reservoir
 
 #################### Task 1 ############################
-    # Request for reservoir dimensions and discretization parameters
+    #Request for reservoir dimensions and discretization parameters
 
-# Lx
-pass
+#Lx
+Lx = float(input('What is the Length of the reservoir in the x axis '))
 # Ly
-pass
+Ly = float(input('What is the Length of the reservoir in the y axis'))
 # Lz
-pass
+Lz = float(input('What is the Length of the reservoir in the z axis'))
 # nx
-pass
+nx = int(input('What is the number of gridblocks in x axis'))
 # ny
-pass
+ny = int(input('What is the number of gridblocks in y axis'))
 # nz
-pass
+nz = int(input('What is the number of gridblocks in z axis'))
 
 
 #################### Task 2 ############################
     # Request for the cut-off value
 # cut_off
-pass
+cut_off= float(input('What is the value of cut_off'))
 
 #################### Task 3 ############################
     # Initialize counters
 
 # n_active
-pass
+n_active = 0
 # n_inactive
-pass
-
+n_inactive = 0
 #################### Task 4 ############################
-    # Loop through all blocks (nested loop)
-for k in range(1, nz+1):
+    # Loop through all blocks (nested loop)          
+for  k in range(1, nz+1):
     # Initialize layer counter
-    pass
+    n_active_layer =0
     # two nested loops go here
-    pass
-
+    for j in range(1, ny+1):
+        for i in range(1, nx+1):
+            perm = float(input("What is the permeability value?"))
+            if perm < cut_off:
+               category = 'inactive'
+               n_inactive = n_inactive + 1
+            else:
+                category = 'active'
+                n_active = n_active + 1
+                n_active_layer = n_active_layer + 1
+            
     # Print layer count
-    pass
+    print(n_active_layer)
 
 #################### Task 5 ############################
-    # Print overall counts
-
-# Print 'active'
-pass
-
-# Print 'inactive'
-pass
+# Print overall counts
+all_blocks = n_active + n_inactive
+n_active_percent = (n_active/all_blocks) * 100
+round_n_active_percent = round(n_active_percent, 2)
+print(f'The percentage of the active blocks in the entire reservoir is {round_n_active_percent}')
